9b3f927f2adeec9ff6995b83fd2c5ff699cbb8be
fuuya-ishida <fuya.ishida@rakus-partners.co.jp>
2022-02-07 17:49:20 +0900

    演習　EmployeeRepository作成（暫定）

---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,97 @@
+package jp.co.sample.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import jp.co.sample.domain.Employee;
+
+public class EmployeeRepository {
+	
+	/** RowMapperの定義 */
+	private static final RowMapper<Employee> Employee_ROW_MAPPER = (rs,i) -> {
+		Employee employee = new Employee();
+		
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getDate("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address "));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("dependents_count"));
+		return employee;
+	};
+	
+	/** template */
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/**
+	 * 従業員一覧情報を取得する
+	 * 
+	 * 入社日の降順
+	 * 従業員が存在しない場合はサイズ０件の従業員一覧を戻り値として設定
+	 */
+	
+	public List<Employee> findAll(){
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,mail_address,"
+				+ "zip_code,address,telephone,salary,characteristics,dependents_count"
+				+ " FROM employees ORDER BY hire_date DESC";
+		
+		try {
+		List<Employee> employeeList = template.query(sql, Employee_ROW_MAPPER);
+		return employeeList;
+		//一件も検索されなかった場合は0件のリストを返す。
+		}catch(Exception e) {
+			return new ArrayList<Employee>();
+		}
+	}
+	
+	/**
+	 * 主キーから従業員情報を取得する
+	 * 
+	 * 
+	 * 従業員が存在しない場合は例外発生
+	 */
+	public Employee load(Integer id) {
+		String sql = "SELECT SELECT id,name,image,gender,hire_date,mail_address,mail_address,"
+				+ "zip_code,address,telephone,salary,characteristics,dependents_count"
+				+ " FROM employees WHERE id = :id";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id",id);
+		
+		Employee employee = template.queryForObject(sql, param, Employee_ROW_MAPPER);
+		
+		return employee;
+	}
+	
+	/**
+	 * 従業員情報を変更する
+	 * 
+	 * 
+	 * 
+	 */
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		
+		String updateSql = "UPDATE employees SET name=:name,image=:image,gender=:gender,hire_date=:hire_date,"
+				+ "mail_address=:mail_address,zip_code=:zip_code,address=:address,telephone=:telephone,"
+				+ "salary=:salary,characteristics=:characteristics,dependents_count=:dependents_count"
+				+ "WHERE name=:name,image=:image,gender=:gender,hire_date=:hire_date,"
+				+ "mail_address=:mail_address,zip_code=:zip_code,address=:address,telephone=:telephone,"
+				+ "salary=:salary,characteristics=:characteristics,dependents_count=:dependents_count";
+		
+		template.update(updateSql, param);
+	}
+}
